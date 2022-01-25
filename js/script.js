ce32b6a1a7d732a2f6d1a2e657d2d93d201188ad
Staryxaizergil <42916293+Staryxaizergil@users.noreply.github.com>
2022-01-25 17:04:22 +0300

    задача №1 JS

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+let lengthArray = 10;
+let arr = [];
+
+for (i = 0; i < lengthArray; i++) {
+	arr.push(Math.round(Math.random() * 100, 1));
+	console.log(arr);
+
+}
+
+for (i = 0; i < arr.length; i++) {
+	newDiv = document.createElement('div');
+	newDiv.innerHTML = "arr[i]";
+	newDiv.style.border = '2px solid blue';
+}
+
