c3e54f88ac7af5a9ad16847f7c3bd063f747730a
Staryxaizergil <42916293+Staryxaizergil@users.noreply.github.com>
2022-01-25 21:53:25 +0300

    задача №2 JS готово

--- js/script.js
+++ js/script.js
@@ -9,19 +9,22 @@ for (i = 0; i < lengthArray; i++) {
 }
 
 for (i = 0; i < arr.length; i++) {
-	newDiv = document.createElement('div');
+	let newDiv = document.createElement('div');
+
+	console.log(newDiv);
+
 
 	if (arr[i] % 2 == 0) {
 		newDiv.innerHTML = `${arr[i]} - четное`;
-	} else newDiv.innerHTML = `${arr[i]} - нечетное`;
+	} else {
+		newDiv.innerHTML = `${arr[i]} - нечетное`;
+	}
 
 	newDiv.style.border = '2px solid blue';
 	document.body.append(newDiv);
+
+	newDiv.addEventListener('click', () => {
+
+		newDiv.remove();
+	})
 }
-
-let elem = document.getElementsByTagName('div');
-
-elem.addEventListener('click', () => {
-	for (let i = 0; i < elem.length; i++)
-		elem.remove();
-})
