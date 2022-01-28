160abe90db68bdc3bc9f19ef573055313c805dd9
Staryxaizergil <42916293+Staryxaizergil@users.noreply.github.com>
2022-01-28 17:38:14 +0300

    lesson Object

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-/*function createBlocks() {
+function createBlocks() {
 	let lengthArray = document.getElementById("chair").value;
 	drawBlocks(lengthArray);
 }
@@ -8,18 +8,25 @@ function drawBlocks(countBlocks) {
 	for (i = 0; i < countBlocks; i++) {
 		let value = Math.round(Math.random() * 100, 1);
 
-		drawBlock(value);
+		let data = {
+			value,
+			isEven: () => {
+				if (value % 2 == 0) {
+					return `четное`;
+				} else {
+					return `нечетное`;
+				}
+			}
+		}
+
+		drawBlock(data);
 	}
 }
 
-function drawBlock(num) {
+function drawBlock(blockInfo) {
 	let newDiv = document.createElement('div');
 
-	if (num % 2 == 0) {
-		newDiv.innerHTML = `${num} - четное`;
-	} else {
-		newDiv.innerHTML = `${num} - нечетное`;
-	}
+	newDiv.innerHTML = `${blockInfo.value} - ${blockInfo.isEven()}`;
 
 	document.body.append(newDiv);
 
@@ -30,30 +37,5 @@ function drawBlock(num) {
 	});
 }
 
-function hiddenText() {
-	let text = document.getElementById('text');
-	console.log(text);
-	text.hidden = true;
-}*/
 
-let menu = document.getElementById('sweets');
-
-function toggleMenu() {
-	let isOpened = menu.classList.contains('open');
-
-	if (isOpened) {
-		menu.classList.remove('open');
-	} else {
-		menu.classList.add('open');
-	};
-}
-
-menu.addEventListener('click', toggleMenu);
-
-
-
-
-/*let lengthArray = document.getElementById('chair').value;
-console.log(lengthArray);
-*/
 
