214ca7715ada255a8affaee6762a402b21fa6e8d
Staryxaizergil <42916293+Staryxaizergil@users.noreply.github.com>
2022-01-28 14:12:45 +0300

    Exersice JS 'open/close menu'

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-function createBlocks() {
+/*function createBlocks() {
 	let lengthArray = document.getElementById("chair").value;
 	drawBlocks(lengthArray);
 }
@@ -30,6 +30,30 @@ function drawBlock(num) {
 	});
 }
 
+function hiddenText() {
+	let text = document.getElementById('text');
+	console.log(text);
+	text.hidden = true;
+}*/
+
+let menu = document.getElementById('sweets');
+
+function toggleMenu() {
+	let isOpened = menu.classList.contains('open');
+
+	if (isOpened) {
+		menu.classList.remove('open');
+	} else {
+		menu.classList.add('open');
+	};
+}
+
+menu.addEventListener('click', toggleMenu);
+
+
+
+
 /*let lengthArray = document.getElementById('chair').value;
 console.log(lengthArray);
 */
+
