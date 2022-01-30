55b1dbbc25693bb89c94f6cb9f69b0925dca7dab
Staryxaizergil <42916293+Staryxaizergil@users.noreply.github.com>
2022-01-30 18:12:50 +0300

    Lesson JS input press Enter

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-
+let inputPress = document.getElementById("chair");
+inputPress.addEventListener("keydown", function (event) {
+	if (event.code === 'Enter') {
+		createBlocks();
+	};
+});
 function createBlocks() {
 	let lengthArray = document.getElementById("chair").value;
 	drawBlocks(lengthArray);
 }
-
 function drawBlocks(countBlocks) {
 	for (i = 0; i < countBlocks; i++) {
 		let value = Math.round(Math.random() * 100, 1);
-
 		let data = {
 			value,
 			isEven: () => {
@@ -21,39 +24,28 @@ function drawBlocks(countBlocks) {
 		drawBlock(data);
 	}
 }
-
 function drawBlock(blockInfo) {
 	let newDiv = document.createElement('div');
 	let outNewDiv = document.createElement('div');
 	let text = blockInfo.isEven() ? 'четное' : 'нечетное';
 
 	newDiv.innerHTML = `${blockInfo.value} - ${text}`;
+	newDiv.classList.add('block');
+
+	outNewDiv.classList.add('main__block');
+	outNewDiv.append(newDiv);
 
 	document.body.append(outNewDiv);
 
-	outNewDiv.classList.add('main__block');
-
-	outNewDiv.append(newDiv);
-
-	newDiv.classList.add('block');
-
 	if (blockInfo.isEven()) {
 		newDiv.classList.add('iseven');
-
+		newDiv.addEventListener('click', (event) => {
+			newDiv.classList.toggle('isvisible');
+		});
 	} else {
 		newDiv.classList.add('isodd');
-
-	};
-
-	if (newDiv.classList.contains('iseven')) {
 		newDiv.addEventListener('click', (event) => {
-			newDiv.classList.toggle('isvisible')
-		});
-	};
-
-	if (newDiv.classList.contains('isodd')) {
-		newDiv.addEventListener('click', (event) => {
-			newDiv.classList.toggle('img')
+			newDiv.classList.toggle('img');
 		});
 	};
 }
